d1ccf2f113e20a1eb7e08f599ac1ea041320dd3e
J0onYEong <cjy2134@naver.com>
2024-03-03 20:57:01 +0900

    [Feat] 모델 뷰모델 기본 구현 (+1 squashed commit) Squashed commits: [8971926] [Feat] 모델 뷰모델 기초구현

---
+++ ShortCapTest/MyShortForm/VIewModel/SSFListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SSFListViewModel.swift
+//  ShortCapTest
+//
+//  Created by 최준영 on 3/3/24.
+//
+
+import Foundation
+
+class SSFListViewModel {
+    
+    var model: [SSFModel]!
+    
+}
---
+++ ShortCapTest/MyShortForm/VIewModel/SSFViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SSFViewModel.swift
+//  ShortCapTest
+//
+//  Created by 최준영 on 3/3/24.
+//
+
+import Foundation
+
+class SSFViewModel {
+    
+    var model: SSFModel!
+    
+}

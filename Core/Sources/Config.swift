dbb3f9b696a212a6ca9ccadd2ca1fca4d86557db
J0onYEong <cjy2134@naver.com>
2024-03-27 13:56:47 +0900

    [Feat] BaseURL을 XCConfig파일로 이동

--- Core/Sources/Config.swift
+++ Core/Sources/Config.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+public enum Config {
+    
+    public enum Network {
+        
+        public static let base: String = {
+            
+            guard let baseUrl = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else { fatalError() }
+            
+            print("✅ baseUrl이 로드되었습빈다!")
+            
+            return baseUrl
+        }()
+    }
+}

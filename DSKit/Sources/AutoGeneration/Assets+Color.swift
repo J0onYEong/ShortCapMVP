9e78178008a8c8e3aa444bc852ebaf834aa17774
J0onYEong <cjy2134@naver.com>
2024-05-10 22:55:03 +0900

    [Feat] SwiftGen을 활용한 리소스자동 코드화 구현
    DSKit모듈의 Resources/Colors.xcassets파일 저장된 색상들을 자동으로 코드화되도록 설정하였습니다.
    생성된 코드는 해당모듈의 Sources/AutoGeneration/Asset+Color.swift로 저장됩니다.

---
+++ DSKit/Sources/AutoGeneration/Assets+Color.swift
@@ -0,0 +1,121 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetColorTypeAlias = ColorAsset.Color
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+internal enum Asset {
+  internal static let black100 = ColorAsset(name: "black100")
+  internal static let black200 = ColorAsset(name: "black200")
+  internal static let black300 = ColorAsset(name: "black300")
+  internal static let black400 = ColorAsset(name: "black400")
+  internal static let g2Blue100 = ColorAsset(name: "G2Blue100")
+  internal static let g2Blue200 = ColorAsset(name: "G2Blue200")
+  internal static let g4Blue100 = ColorAsset(name: "G4Blue100")
+  internal static let g4Blue200 = ColorAsset(name: "G4Blue200")
+  internal static let g4Blue300 = ColorAsset(name: "G4Blue300")
+  internal static let g4Blue400 = ColorAsset(name: "G4Blue400")
+  internal static let mainBlue100 = ColorAsset(name: "mainBlue100")
+  internal static let mainBlue200 = ColorAsset(name: "mainBlue200")
+  internal static let mainBlue300 = ColorAsset(name: "mainBlue300")
+  internal static let mainAqua100 = ColorAsset(name: "mainAqua100")
+  internal static let mainAqua200 = ColorAsset(name: "mainAqua200")
+  internal static let subLightGreen = ColorAsset(name: "subLightGreen")
+  internal static let subLightYellow = ColorAsset(name: "subLightYellow")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  internal func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  internal private(set) lazy var swiftUIColor: SwiftUI.Color = {
+    SwiftUI.Color(asset: self)
+  }()
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+internal extension SwiftUI.Color {
+  init(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+}
+#endif
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
